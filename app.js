3003cccfdff60e5d1ac63c1b0abc950dc6628611
Ciaran <cguhagil@terpmail.umd.edu>
2018-07-06 13:43:18 -0600

    fetch and stringify - ciaran

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// yo
+let url = './planets.json'
+fetch(url)
+    .then(response => response.json())
+    .then(myData => console.log(myData.data))
+    //.then(myData => addInfo(myData))
+
+// function addInfo(data) {
+
+// }
+
+
+
+console.log('ello')
+
