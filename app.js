771aa9efae17b2ad030da0bf847f46228edfb8a3
Ciaran <cguhagil@terpmail.umd.edu>
2018-07-06 14:43:56 -0600

    adding app.js -ciaran

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 let url = './planets.json'
 fetch(url)
     .then(response => response.json())
-    .then(myData => console.log(myData.data))
-    //.then(myData => addInfo(myData))
+    //.then(myData => console.log(myData.data))
+    .then(myData => addInfo(myData.data))
 
-// function addInfo(data) {
-
-// }
-
-
-
-console.log('ello')
-
+function addInfo(data) {
+    let allPlanets = document.querySelectorAll('img')
+    for(let i=0; i<data.length; i++){
+        allPlanets[i].setAttribute("name",data[i].gasA)
+        allPlanets[i].addEventListener("click", function(event){
+            console.log(event.target.name)
+            alert(event.target.name)
+        })
+}}
